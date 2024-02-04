53046014063a74bdfdb0ccea0e3c76f19ccdd552
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2024-02-04 14:06:17 +0100

    edit the  app page

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './containers/Home';
-import About from './containers/About/AboutMe';
+// import About from './containers/About/AboutMe';
 import Project from './containers/Projects/Project';
 import Skills from './containers/Skills/Skill';
 import Experience from './containers/Experiences/Experience';
@@ -16,7 +16,7 @@ const AppRoutes = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        {/* <Route path="/about" element={<About />} /> */}
         <Route path="/projects" element={<Project />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/experience" element={<Experience />} />
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,10 +5,22 @@ import './Home.css';
 function Home() {
   return (
     <div className="home-container">
+      <div className="content-container">
         <div className="content">
-        <h3>Welcome to My Portfolio</h3>
-        <p>Explore the various facets of my work and skills. Whether it's web development, mobile applications, or design, I'm passionate about creating exceptional experiences. Feel free to navigate through the pages and learn more about my journey!</p>
+          <h3>Welcome to My Portfolio</h3>
+          <p>Embark on a journey through the diverse realms of my work and skills. From web development and mobile applications to design, I am fueled by a passion for crafting exceptional digital experiences. Navigate through the pages to delve deeper into my professional journey!</p>
         </div>
+
+        <div className="content">
+          <h3>About Me</h3>
+          <p>
+            I am Omar EL HASSANI, a dedicated and passionate web and mobile developer, certified in AppSec (CAP), with a knack for crafting intuitive UX/UI experiences. Hailing from Marrakesh-Safi, Morocco, I bring a unique blend of technical expertise and artistic sensibility to every project.
+          </p>
+          <p>
+            My journey in computer engineering has equipped me with a strong foundation, and I am always eager to explore new opportunities to expand my skills and stay abreast of the latest technological trends. I approach each project with the goal of providing efficient technical solutions while infusing creativity into the design process.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
