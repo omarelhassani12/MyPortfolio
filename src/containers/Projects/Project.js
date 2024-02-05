4f05aa0cfec04862af5a5b652f9bdd879dbe358c
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2024-02-05 17:37:49 +0100

    complete the project page and fix some issuse

--- src/containers/Projects/Project.js
+++ src/containers/Projects/Project.js
@@ -1,54 +1,62 @@
-// Projects.js
+// Project.js
 
 import React, { useState } from 'react';
 import './Project.css';
+import ofpptImage from '../../assets/companies/ofppt.png';
+
+
+const allProjects = [
+  { id: 0, title: 'Tabibi', category: 'Mobile', image: ofpptImage, url: 'https://github.com/omarelhassani12/Tabibi' },
+  { id: 1, title: 'Absence Management System', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/gestion_d_absence' },
+  { id: 2, title: 'Medical Cabinet System', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/Medical_Web_site' },
+  { id: 3, title: 'Appointify', category: 'Mobile', image: ofpptImage, url: 'https://github.com/omarelhassani12/Appointify' },
+  { id: 4, title: 'Saska', category: 'Design', image: ofpptImage, url: '#' },
+  { id: 5, title: 'Stock Management System', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/Management-stock' },
+  { id: 6, title: 'Scentit', category: 'Web', image: ofpptImage, url: 'https://omarelhassani12.github.io/Scentit/' },
+  { id: 7, title: 'Tabibi Web', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/Tabibi_panel' },
+  { id: 8, title: 'Restaurant Reservation', category: 'Design', image: ofpptImage, url: 'https://github.com/omarelhassani12/restaurant_reservation_flutter_app' },
+  { id: 9, title: 'BrainWave', category: 'Mobile', image: ofpptImage, url: 'https://github.com/omarelhassani12/school_app' },
+  { id: 10, title: 'AsrooShop', category: 'Mobile', image: ofpptImage, url: 'https://github.com/omarelhassani12/AsrooShop-using-flutter-and-firebase' },
+  { id: 11, title: 'Interns Management System', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/Stagiares-management-' },
+  { id: 12, title: 'Staffs Management System', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/SimpleStaffManagement' },
+];
 
 function Projects() {
-  const [activeFilter, setActiveFilter] = useState('all'); // Default filter
+  const [activeFilter, setActiveFilter] = useState('All');
 
-  const handleFilterChange = (filter) => {
-    setActiveFilter(filter);
-  };
-
-  const projectsData = [
-    { title: 'Project 1', category: 'all', description: 'Description for Project 1' },
-    { title: 'Project 2', category: 'category1', description: 'Description for Project 2' },
-    { title: 'Project 2', category: 'category1', description: 'Description for Project 2' },
-    { title: 'Project 2', category: 'category1', description: 'Description for Project 2' },
-    { title: 'Project 3', category: 'category2', description: 'Description for Project 3' },
-    { title: 'Project 3', category: 'category2', description: 'Description for Project 3' },
-    { title: 'Project 3', category: 'category2', description: 'Description for Project 3' },
-    { title: 'Project 3', category: 'category2', description: 'Description for Project 3' },
-    { title: 'Project 2', category: 'category1', description: 'Description for Project 2' },
-    { title: 'Project 2', category: 'category1', description: 'Description for Project 2' },
-    { title: 'Project 2', category: 'category1', description: 'Description for Project 2' },
-    { title: 'Project 3', category: 'category2', description: 'Description for Project 3' },
-    // Add more project data
-  ];
-
-  const filteredProjects = activeFilter === 'all'
-    ? projectsData
-    : projectsData.filter(project => project.category === activeFilter);
+  const filteredProjects = activeFilter === 'All'
+    ? allProjects
+    : allProjects.filter(project => project.category === activeFilter);
 
   return (
-    <section id="projects" className="project-container">
-      <div className="project-filter">
-        <button onClick={() => handleFilterChange('all')} className={activeFilter === 'all' ? 'active' : ''}>All</button>
-        <button onClick={() => handleFilterChange('category1')} className={activeFilter === 'category1' ? 'active' : ''}>Category 1</button>
-        <button onClick={() => handleFilterChange('category2')} className={activeFilter === 'category2' ? 'active' : ''}>Category 2</button>
-        {/* Add more buttons for other categories */}
+    <div className="projects-container">
+      <div className="filter-container">
+        <select
+          id="filter-select"
+          onChange={(e) => setActiveFilter(e.target.value)}
+          value={activeFilter}
+        >
+          <option value="All">All</option>
+          <option value="Web">Web</option>
+          <option value="Mobile">Mobile</option>
+          <option value="Design">Design</option>
+        </select>
       </div>
 
-      {/* Display filtered projects */}
       <div className="projects-list">
         {filteredProjects.map((project, index) => (
-          <div key={index} className="project-item">
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
+          <div key={index} className="project-card">
+            <a href="#">
+              <div className="project-background" style={{ backgroundImage: `url(${project.image})` }}></div>
+              <div className="project-content">
+                <h3><a href={project.url}>{project.title}</a></h3>
+                <span className="category-tag">{project.category}</span>
+              </div>
+            </a>
           </div>
         ))}
       </div>
-    </section>
+    </div>
   );
 }
 
