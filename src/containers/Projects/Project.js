6499cee9151dac6ee1cd799d1015fcfdbfa5c120
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2024-02-07 15:46:41 +0100

    make the projectDetailsModal and add some other functionalties

--- src/containers/Projects/Project.js
+++ src/containers/Projects/Project.js
@@ -1,34 +1,19 @@
-// Project.js
-
-import React, { useState, useEffect  }     from 'react';
-import './Project.css';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
+import { allProjects } from './projectData';
+
+import './Project.css';
+import './ProjectDetailsModal.css';
 
 
-
-import ofpptImage from '../../assets/companies/devosoft.png';
 const projectsPerPage = 12;
-
-const allProjects = [
-  { id: 0, title: 'Tabibi', category: 'Mobile', image: ofpptImage, url: 'https://github.com/omarelhassani12/Tabibi' },
-  { id: 1, title: 'Absence Management System', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/gestion_d_absence' },
-  { id: 2, title: 'Medical Cabinet System', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/Medical_Web_site' },
-  { id: 3, title: 'Appointify', category: 'Mobile', image: ofpptImage, url: 'https://github.com/omarelhassani12/Appointify' },
-  { id: 4, title: 'Saska', category: 'Design', image: ofpptImage, url: '#' },
-  { id: 5, title: 'Stock Management System', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/Management-stock' },
-  { id: 6, title: 'Scentit', category: 'Web', image: ofpptImage, url: 'https://omarelhassani12.github.io/Scentit/' },
-  { id: 7, title: 'Tabibi Web', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/Tabibi_panel' },
-  { id: 8, title: 'Restaurant Reservation', category: 'Design', image: ofpptImage, url: 'https://github.com/omarelhassani12/restaurant_reservation_flutter_app' },
-  { id: 9, title: 'BrainWave', category: 'Mobile', image: ofpptImage, url: 'https://github.com/omarelhassani12/school_app' },
-  { id: 10, title: 'AsrooShop', category: 'Mobile', image: ofpptImage, url: 'https://github.com/omarelhassani12/AsrooShop-using-flutter-and-firebase' },
-  { id: 11, title: 'Interns Management System', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/Stagiares-management-' },
-  { id: 12, title: 'Staffs Management System', category: 'Web', image: ofpptImage, url: 'https://github.com/omarelhassani12/SimpleStaffManagement' },
-];
-
 
 function Projects() {
   const [activeFilter, setActiveFilter] = useState('All');
   const [currentPage, setCurrentPage] = useState(1);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
   const filteredProjects = activeFilter === 'All'
     ? allProjects
@@ -48,42 +33,32 @@ function Projects() {
   const endIndex = startIndex + projectsPerPage;
   const displayedProjects = filteredProjects.slice(startIndex, endIndex);
 
+  // Function to handle opening the modal and setting the selected project
+  const handleProjectClick = (project) => {
+    setSelectedProject(project);
+    setModalIsOpen(true);
+  };
 
-  //for the model
-  const [modalIsOpen, setModalIsOpen] = useState(true);
+  const handleModalClose = () => {
+    setModalIsOpen(false);
+    setSelectedProject(null);
+    setSelectedImageIndex(0);
+  };
 
+  const handleNextImage = () => {
+    if (selectedProject?.images) {
+      setSelectedImageIndex(selectedImageIndex === selectedProject.images.length - 1 ? 0 : selectedImageIndex + 1);
+    }
+  };
 
-  useEffect(() => {
-    // Close the modal after 3 seconds
-    const timeout = setTimeout(() => {
-      setModalIsOpen(false);
-    }, 1000);
+  const handlePreviousImage = () => {
+    if (selectedProject?.images) {
+      setSelectedImageIndex(selectedImageIndex === 0 ? selectedProject.images.length - 1 : selectedImageIndex - 1);
+    }
+  };
 
-    // Cleanup function to clear the timeout
-    return () => clearTimeout(timeout);
-  }, []);
-
-
-  
   return (
     <div className="projects-container">
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-        contentLabel="Alert"
-        style={{
-          content: {
-            width: '25%',
-            height: '10%',
-            margin: 'auto',
-            color: 'red', 
-            borderRadius: '20px', 
-          }
-        }}
-      >
-        <h4>To view details, please click on the project title.</h4>
-      </Modal>
-
       <div className="filter-container">
         <select
           id="filter-select"
@@ -100,43 +75,16 @@ function Projects() {
       <div className="projects-list">
         {displayedProjects.map((project, index) => (
           <div key={index} className="project-card">
-            <a href={project.url}>
+            <a href='#' onClick={() => handleProjectClick(project)}>
               <div className="project-background" style={{ backgroundImage: `url(${project.image})` }}></div>
               <div className="project-content">
-                <h3><a href='#'>{project.title}</a></h3>
+                <h3><a href={project.url}>{project.title}</a></h3>
                 <span className="category-tag">{project.category}</span>
               </div>
             </a>
           </div>
         ))}
       </div>
-      
-      {/* <div className="projects-list">
-        {displayedProjects.map((project, index) => (
-          <div key={index} className="project-card">
-            <a href={project.url}>
-              <div className="project-background" style={{ backgroundImage: `url(${project.image})` }}></div>
-              <div className="project-content">
-                <h3><a href='#'>{project.title}</a></h3>
-                <span className="category-tag">{project.category}</span>
-              </div>
-            </a>
-            <div className="project-icons">
-              {project.url && project.url.startsWith('https://github.com') && (
-                <a href={project.url} target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-github github-icon"></i>
-                </a>
-              )}
-              {project.url && !project.url.startsWith('https://github.com') && (
-                <a href={project.url} target="_blank" rel="noopener noreferrer">
-                  <i className="fas fa-external-link-alt link-icon"></i>
-                </a>
-              )}
-            </div>
-
-          </div>
-        ))}
-      </div> */}
 
       <div className="pagination">
         <div>
@@ -145,8 +93,74 @@ function Projects() {
         <div className="paginationButtons">
           <button onClick={handlePreviousPage} className={currentPage === 1 ? 'notAnyPage' : 'paginationButton'}>Back</button>
           <button onClick={handleNextPage} className={currentPage === totalPages ? 'notAnyPage' : 'paginationButton'}>Next</button>
+        </div>
       </div>
-      </div>
+
+      {/* Modal for displaying project details */}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={handleModalClose}
+        className="modal-content"
+        overlayClassName="modal-overlay"
+        ariaHideApp={false}
+      >
+        <div className="modal-header">
+          <h2>{selectedProject?.title}</h2>
+          <div className="modal-icons">
+            {selectedProject && (
+              <>
+                <a href={selectedProject.url} target="_blank" rel="noopener noreferrer" className="icon-link">
+                  <i className="fas fa-external-link-alt icon"></i> <span>View Project</span>
+                </a>
+                <a href={selectedProject.githubUrl} target="_blank" rel="noopener noreferrer" className="icon-link">
+                  <i className="fab fa-github icon"></i> <span>View GitHub</span>
+                </a>
+              </>
+            )}
+          </div>
+          <button className="close-button" onClick={handleModalClose}>X</button>
+        </div>
+        <div className="modal-body content-container">
+          <div className="modal-left content">
+            <div className="modal-top-half">
+            <h4>About {selectedProject?.title}</h4>
+              {selectedProject?.description && selectedProject.description.map((desc, index) => (
+                <div key={index}>{desc}</div>
+              ))}
+            </div>
+            <hr/>
+            <div className="modal-bottom-half">
+              <h4>Tools</h4>
+              <div className="tools-list">
+                {selectedProject?.tools && selectedProject.tools.map((tool, index) => (
+                  <span key={index} className="tool" style={{ color: `#${Math.floor(Math.random()*16777215).toString(16)}`,marginRight: '10px' }}>
+                    #{tool} 
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="modal-right content">
+            <div className="image-container">
+              {selectedProject?.images &&
+                <img src={selectedProject.images[selectedImageIndex]} alt={selectedProject?.title} />
+              }
+              <div className="navigation-buttons">
+                {selectedProject?.images && selectedProject.images.length > 1 &&
+                  <>
+                    <a href='#' onClick={handlePreviousImage}>
+                      <i className="fas fa-chevron-left"></i>
+                    </a>
+                    <a href='#' onClick={handleNextImage}>
+                      <i className="fas fa-chevron-right"></i>
+                    </a>
+                  </>
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
