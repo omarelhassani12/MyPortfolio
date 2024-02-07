9c78dfee0ad332393d225969a19a4acb39748bd5
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2024-02-08 00:29:37 +0100

    complete the certificate view and thier data

--- src/containers/Certificates/Certificate.js
+++ src/containers/Certificates/Certificate.js
@@ -1,16 +1,39 @@
-// Projects.js
-
+// Certificates.js
 import React from 'react';
 import './Certificate.css';
 
+function Certificates() {
+  const certificates = [
+    {
+      name: "Certified Application Security Practitioner(CAP)",
+      issuer: "The SecOps Group",
+      date: "February 2024",
+      backgroundImage: require('../../assets/certificates/CAP.png')
+    },
+    {
+      name: "Certificate in JavaScript Fundamentals",
+      issuer: "ABC Institute",
+      date: "March 2022",
+      backgroundImage: "url('path/to/javascript_certificate.jpg')"
+    },
+    // Add more certificate objects with background images as needed
+  ];
 
-function Projects() {
   return (
-    <section id="project">
-      <h2>Certificates</h2>
-      {/* Your projects content goes here */}
+    <section id="certificates">
+      <div className="certificates-list">
+        {certificates.map((certificate, index) => (
+          <div className="certificate-item" key={index} style={{ backgroundImage: `url(${certificate.backgroundImage})` }}>
+            <div className="certificate-text">
+              <h3>{certificate.name}</h3>
+              <p>Issuer: <b>{certificate.issuer}</b></p>
+              <p>Date of Issuance: <b>{certificate.date}</b></p>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
 
-export default Projects;
+export default Certificates;
